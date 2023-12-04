db782fa834f2430c97d299c51e1d1f7b073594dc
Bonevy BEBY <bonevybeby@gmail.com>
2023-12-04 07:52:06 +0100

    add basic auth with test in IJ

--- security/src/main/java/com/formationkilo/security/web/TestRestAPI.java
+++ security/src/main/java/com/formationkilo/security/web/TestRestAPI.java
@@ -1,11 +1,13 @@
 package com.formationkilo.security.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
 public class TestRestAPI {
+    @GetMapping("/dataTest")
     public Map<String, Object>dataTest(){
         return Map.of("message","Data test");
     }
