4c2e18d0d91b8896529ef68a556615ac4ee31b0a
Bonevy BEBY <bonevybeby@gmail.com>
2023-12-09 08:39:15 +0100

    use basicless authentication by affecting the token

--- security/src/main/java/com/formationkilo/security/config/SecurityConfig.java
+++ security/src/main/java/com/formationkilo/security/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
           http
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                         .requestMatchers(AntPathRequestMatcher.antMatcher("/dataTest")).permitAll()
+                //         .requestMatchers(AntPathRequestMatcher.antMatcher("/dataTest")).permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
--- security/src/main/java/com/formationkilo/security/web/TestRestAPI.java
+++ security/src/main/java/com/formationkilo/security/web/TestRestAPI.java
@@ -1,5 +1,6 @@
 package com.formationkilo.security.web;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,16 @@ public class TestRestAPI {
     @GetMapping("/dataTest")
     public Map<String, Object>dataTest(){
         return Map.of("message","Data Test");
+    }
+
+    @GetMapping("/dataTest2")
+    public Map<String, Object>dataTest2(Authentication authentication){
+
+        return Map.of(
+                "message","Data Test",
+                "username", authentication.getName(),
+                "authorities",authentication.getAuthorities()
+
+        );
     }
 }
