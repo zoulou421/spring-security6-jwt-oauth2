c66c97283eba3861d59422534f9a3af80932cf5d
Bonevy BEBY <bonevybeby@gmail.com>
2023-12-13 13:16:31 +0100

    add Exceptions

--- security/src/main/java/com/formationkilo/security/web/AuthController.java
+++ security/src/main/java/com/formationkilo/security/web/AuthController.java
@@ -4,6 +4,8 @@ package com.formationkilo.security.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -107,8 +109,8 @@ public class AuthController {
    }
 
    //update: String grantType,refreshToken ADDED
-   @PostMapping("/token")
-   public Map<String,String>jwtToken(String grantType,
+   @PostMapping("/token5")
+   public Map<String,String>jwtToken5(String grantType,
                                      String username,
                                      String password,
                                      boolean withRefreshToken,
@@ -158,4 +160,66 @@ public class AuthController {
       }
       return idToken;
    }
+    //update: String grantType,refreshToken ADDED
+    @PostMapping("/token")
+    public ResponseEntity<Map<String,String>> jwtToken(String grantType,
+                                                       String username,
+                                                       String password,
+                                                       boolean withRefreshToken,
+                                                       String refreshToken){
+        String  subject=null;String scope=null;
+        //Authentication authentication=null;
+        if(grantType!="" && grantType.equals("password")){
+            Authentication authentication=authenticationManagerBean.authenticate(
+                    new UsernamePasswordAuthenticationToken(username,password)
+            );
+            subject=authentication.getName();
+            scope=authentication.getAuthorities().stream().map(aut->aut.getAuthority())
+                    .collect(Collectors.joining(" "));
+        }else if(grantType!="" && grantType.equals("refreshToken")){
+            //added part
+            if(refreshToken==null){
+               return new ResponseEntity<>(Map.of("errorMessage","refreshToken is required"), HttpStatus.UNAUTHORIZED);
+            }
+            Jwt decodeJWT= null;
+            try {
+                decodeJWT = jwtDecoder.decode(refreshToken);
+            } catch (JwtException e) {
+                return new ResponseEntity<>(Map.of("errorMessage",e.getMessage()), HttpStatus.UNAUTHORIZED);
+               // throw new RuntimeException(e);
+            }
+            subject=decodeJWT.getSubject();
+            UserDetails userDetails=userDetailsService.loadUserByUsername(subject);
+            Collection<? extends GrantedAuthority> authorities=userDetails.getAuthorities();
+            scope=authorities.stream().map(aut->aut.getAuthority()).collect(Collectors.joining(" "));
+        }
+        Map<String,String>idToken=new HashMap<>();
+        Instant instant=Instant.now();
+        //Collection<? extends GrantedAuthority>authorities=authentication.getAuthorities()
+        // String scope=authentication.getAuthorities().stream().map(aut->aut.getAuthority())
+        //        .collect(Collectors.joining(" "));
+        JwtClaimsSet jwtClaimsSet=JwtClaimsSet.builder()
+                .subject(subject)
+                .issuedAt(instant)//actuel date or system date
+                .expiresAt(instant.plus(withRefreshToken?1:5, ChronoUnit.MINUTES))
+                .issuer("security")// security represent the app name that generated the token
+                .claim("scope",scope)
+                .build();
+        String jwtAcessToken=jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
+        idToken.put("accessToken",jwtAcessToken);
+        if(withRefreshToken){
+            JwtClaimsSet jwtClaimsSetRefresh=JwtClaimsSet.builder()
+                    .subject(subject)
+                    .issuedAt(instant)//actuel date or system date
+                    .expiresAt(instant.plus(5, ChronoUnit.MINUTES))
+                    .issuer("security")// security represent the app name that generated the token
+                    //.claim("scope",scope) you don't need to send roles.
+                    .build();
+            String jwtRefreshToken=jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSetRefresh)).getTokenValue();
+            //add this:
+            idToken.put("refreshToken",jwtRefreshToken);
+
+        }
+        return new ResponseEntity<>(idToken,HttpStatus.OK);
+    }
 }
